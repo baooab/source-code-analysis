f329797c22f171bc377e3e28e2c1872211636e93
zhangbao <zhangbao90s@qq.com>
2023-09-22 08:26:46 +0800

    update van.js

--- van.js/van.js
+++ van.js/van.js
@@ -32,6 +32,26 @@ let stateProto = {
   }
 }
 
+let state = initVal => ({
+  __proto__: stateProto,
+  _val: initVal,
+  oldVal: initVal,
+  bindings: [],
+  // listeners: [],
+})
+
+let toDom = v => v.nodeType ? v : new Text(v)
+
+let add = (dom, ...children) => {
+  children.forEach(child => {
+    const appendChild = protoOf(child) === stateProto ? bind(child, v => v) : toDom(child)
+    // !
+    dom.appendChild(appendChild)
+  })
+  
+  return dom
+}
+
 let filterBindings = s => {
   s.bindings = s.bindings.filter(b => b.dom?.isConnected)
   return s.bindings
@@ -50,10 +70,19 @@ let updateDoms = () => {
   changedStatesArray.forEach(s => s.oldVal = s._val)
 }
 
-let state = initVal => ({
-  __proto__: stateProto,
-  _val: initVal,
-  oldVal: initVal,
-  bindings: [],
-  // listeners: [],
-})
+let bind = (...args) => {
+  // deps: state/prop, func: handler function(`state._val`)
+  let deps = args.slice(0, -1), func = args[args.length - 1]
+  let result = func(...deps.map(d => d._val))
+  if (result == undefined) return [] // ??
+  let binding = {_deps: deps, dom: toDom(result), func}
+  deps.forEach(s => {
+    // statesToGc = addAndScheduleOnFirst(statesToGc, s,
+    //   () => (statesToGc.forEach(filterBindings), statesToGc = undefined),
+    //   bindingGcCycleInMs)
+
+    // bindings for every state/prop
+    s.bindings.push(binding)
+  })
+  return binding.dom
+}
